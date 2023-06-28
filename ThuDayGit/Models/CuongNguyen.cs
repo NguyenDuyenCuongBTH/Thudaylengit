f054a8181e04a6235048971c1587c41d8caeb6ac
NDC114 <duyencuongmtpvip2001@gmail.com>
2023-06-28 21:47:21 +0700

    Daylan2

---
+++ ThuDayGit/Models/CuongNguyen.cs
@@ -0,0 +1,9 @@
+namespace ThuDayGit
+{
+    public class CuongNguyen
+    {
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public int Sdt { get; set; }
+    }
+}
